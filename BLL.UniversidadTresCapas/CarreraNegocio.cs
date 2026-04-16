acaaabfa70416ff1e259adda552047576852f79b
Yaggi <yaggim@gmail.com>
2026-04-16 18:03:46 -0300

    Se agregan excepciones de negocio y se corrige solucion

--- BLL.UniversidadTresCapas/CarreraNegocio.cs
+++ BLL.UniversidadTresCapas/CarreraNegocio.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DAL.UniversidadTresCapas;
+using BLL.UniversidadTresCapas.Excepciones;
 
 namespace BLL.UniversidadTresCapas
 {
@@ -24,7 +25,8 @@ namespace BLL.UniversidadTresCapas
 
 		public bool CrearCarrera(CarreraBE carreraEntidad)
 		{
-			DAL.UniversidadTresCapas.MateriaDAL materiaDAL = new DAL.UniversidadTresCapas.MateriaDAL();
+            ValidarCarrera(carreraEntidad);
+            DAL.UniversidadTresCapas.MateriaDAL materiaDAL = new DAL.UniversidadTresCapas.MateriaDAL();
 			return materiaDAL.CrearCarrera(carreraEntidad);
 		}
 
@@ -37,5 +39,26 @@ namespace BLL.UniversidadTresCapas
 			return materiaDAL.ListarMateriasPorCarrera(carreraEntidad);
 
 		}
+
+		public void ValidarCarrera(CarreraBE carreraEntidad)
+		{
+            if (string.IsNullOrEmpty(carreraEntidad.Nombre))
+            {
+                throw new ExcepcionDeNegocio("El nombre de la carrera no puede ser vacío");
+            }
+            if (string.IsNullOrEmpty(carreraEntidad.NombreDecano))
+            {
+                throw new ExcepcionDeNegocio("El nombre del decano no puede ser vacío");
+            }
+            if (carreraEntidad.Nombre.Length < 5)
+            {
+                throw new ExcepcionDeNegocio("El nombre de la carrera no puede tener menos de 5 caracteres");
+            }
+            if (carreraEntidad.Nombre.Length > 50)
+            {
+                throw new ExcepcionDeNegocio("El nombre de la carrera no puede superar los 50 caracteres");
+            }
+
+        }
 	}
 }
